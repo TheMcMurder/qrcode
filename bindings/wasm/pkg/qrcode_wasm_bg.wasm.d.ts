c9ba3a7b1aa2317e757bb246565ca11ad6dbe457
the_mcmurder <3059715+TheMcMurder@users.noreply.github.com>
2025-05-22 15:09:08 -0600

    turns out I need this

---
+++ bindings/wasm/pkg/qrcode_wasm_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const hello: () => [number, number];
+export const generate_qr_svg: (a: number, b: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
